490891dad6a00836f117bb838451f92e5b3e39b2
muhammad <hassan@Hassans-MacBook-Air.local>
2020-09-03 17:02:51 -0400

    fixed links

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import  "bulma/css/bulma.min.css"
 const App = () => {
     return (
         <div>
-            <nav class="navbar  is-success is-fixed-top" style = {{backgroundColor:"#003049"}} >
+            <nav class="navbar  is-success is-fixed-top" style = {{backgroundColor:"black"}} >
             </nav>
-            <section class="hero is-fullheight is-light" >
+            <section class="hero is-fullheight is-white" >
                 <div class="hero-body">
                     <div class="container has-text-centered"   >
                         <div class="column is-4 is-offset-4" >
-                            <h1 class="title" style={{ color: "black",fontWeight:800 , marginBottom:30, fontSize:30 }} >Welcome to E-Learning Classroom </h1>
+                            <h1 class="title" style={{ color: "black",fontWeight:600 , marginBottom:30, fontSize:30 ,}} >Welcome to E-Learning Portal </h1>
                             <h1 class="subtitle" style={{ color: "black",fontWeight:300 , marginBottom:30, fontSize:24 }}> Login using your School ID</h1>
-                            <a href = "http://edubase.cc/login/index.php/" class="button is-medium is-rounded is-fullwidth"  style = {{backgroundColor:"#003049" , color:"white"}}> Login</a>
-                            <h1 class="subtitle" style={{ color: "black",fontWeight:600 , marginBottom:10, fontSize:24 , marginTop:60 }}> Need Help ? </h1>
-                            <a href = "http://edubase.cc/login/index.php/" class="button is-medium is-rounded is-fullwidth is-outlined"  style = {{backgroundColor:"#003049" , color:"white"}}> Contact Us</a>
+                            <a href = "http://elearning.edubase.cc/login/index.php/" class="button is-large is-rounded is-fullwidth"  style = {{backgroundColor:"black" , color:"white"}}> Login</a>
+                            <h1 class="subtitle" style={{ color: "black",fontWeight:600 , marginBottom:10, fontSize:24 , marginTop:60 }}> Need Help ?</h1>
+                            <a href = "mailto:hafeez.admc@gmail.com" class="button is-medium is-rounded is-large is-fullwidth is-outlined"  style = {{backgroundColor:"black" , color:"white"}}> Contact Us </a>
                         </div>
                     </div>
                 </div>
